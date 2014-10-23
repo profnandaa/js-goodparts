820a2f06b8bf485d74666db1ca349f71a0488594
ProfNandaa <profnandaa@gmail.com>
2014-10-23 05:34:41 +0300

    Starting chap. 2

---
+++ js/ch2-grammar.js
@@ -0,0 +1,37 @@
+/*I know it well:
+I read it in the grammar long ago.
+—William Shakespeare, The Tragedy of Titus Andronicus
+*/
+
+/*White spaces */
+
+// /*  */ are not safe for commenting out blocks of code,
+// esp. for code with regular expression e.g.
+
+/*
+var rm_a = /a*/.match(s);
+*/
+
+/*Names */
+/* reserved names:
+abstract
+boolean break byte
+case catch char class const continue
+debugger default delete do double
+else enum export extends
+false final finally float for function
+goto
+if implements import in instanceof int interface
+long
+native new null
+package private protected public
+return
+short static super switch synchronized
+this throw throws transient true try typeof
+var volatile void
+while with
+
+*/
+
+
+
