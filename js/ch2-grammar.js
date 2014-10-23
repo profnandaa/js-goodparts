d2fc2154386d6f83c8ffa8c636e5fba8682a2612
ProfNandaa <profnandaa@gmail.com>
2014-10-23 05:56:12 +0300

    Grammer: Numbers

--- js/ch2-grammar.js
+++ js/ch2-grammar.js
@@ -8,9 +8,9 @@ I read it in the grammar long ago.
 // /*  */ are not safe for commenting out blocks of code,
 // esp. for code with regular expression e.g.
 
-/*
-var rm_a = /a*/.match(s);
-*/
+//	/*
+//	var rm_a = /a*/.match(s);
+//	*/
 
 /*Names */
 /* reserved names:
@@ -30,6 +30,25 @@ short static super switch synchronized
 this throw throws transient true try typeof
 var volatile void
 while with
+
+*/
+
+/*Numbers*/
+
+/*
+- JS has a single number type. Internally represented as a 64-bit floating
+ point, the same as Java's double.
+
+- Upside: problems of overlow in short intergets are completely avoided;
+	a large class of number type errors are avoided.
+
+- Value NaN is a number value that is the result of an operation that 
+cannot produce a normal result. NaN is not equal to any value, including 
+itself! You can detect NaN with the isNaN(number) function.
+
+- JS has a Math object that contains a set of methods that act on numbers. 
+e.g. Math.floor(number) method can be used to convert a number into 
+an integer
 
 */
 
