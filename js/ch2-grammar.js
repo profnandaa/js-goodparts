6a94ad74c1f4fc864e9073a803cedc5480fdd9b1
ProfNandaa <profnandaa@gmail.com>
2014-10-24 04:05:19 +0300

    Functions

--- js/ch2-grammar.js
+++ js/ch2-grammar.js
@@ -106,6 +106,14 @@ Types of literals:
 
 */
 
+/* == Functions == */
+
+/*
+"A function literal defines a function value. It can have an optional name that it can
+use to call itself recursively. It can specify a list of parameters that will act as vari-
+ables initialized by the invocation arguments. The body of the function includes vari-
+able definitions and statements. There will be more about functions in Chapter 4.
+"
 
 
 
