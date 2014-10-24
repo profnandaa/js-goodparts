ec2946b27b72b5c01faa96cd4e6a4f2b0da41926
ProfNandaa <profnandaa@gmail.com>
2014-10-25 02:12:03 +0300

    Delete

--- js/ch3-objects.js
+++ js/ch3-objects.js
@@ -275,6 +275,26 @@ for(i=0; i<properties.length; i += 1){
 }
 
 
+/* == Delete == */
+/*
+* The delete operator can be used to remove a property from an object.
+	It will remove a property from the object if it has one. It will 
+	not touch any of the objects in the prototype linkage.
+
+* Removing a property from an object may allow a property from the 
+	prototype linkage to shine through:
+*/
+
+console.log(another_stooge.nickname);		//'Moe'
+//Remove nickname from another_stooge, 
+//revealing the nickname of the prototype
+
+delete another_stooge.nickname;
+
+console.log(another_stooge.nickname);		// 'Curly'
+
+
+
 
 
 
