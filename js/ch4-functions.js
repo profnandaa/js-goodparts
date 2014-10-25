83139746912fb71df289e306aa9ce93a86e5b015
ProfNandaa <profnandaa@gmail.com>
2014-10-25 07:32:42 +0300

    Introduction to Functions

---
+++ js/ch4-functions.js
@@ -0,0 +1,20 @@
+/*
+Why, every fault’s condemn’d ere it be done:
+Mine were the very cipher of a function...
+—William Shakespeare, Measure for Measure
+*/
+
+/*
+* The best thing about JS is its implementation of functions.
+	It got almost everything right.
+
+* A function encloses a set of statements. Functions are 
+	fundamental modular unit of JS.
+* They are use for:
+	- code resue
+	- information hiding, and
+	- composition
+	* to specify the behavior of objects
+
+*/
+
