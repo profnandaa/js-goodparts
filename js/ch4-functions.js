9b2771ee1f6e649b5b5c5653b87f1148c3ce68c3
ProfNandaa <profnandaa@gmail.com>
2014-10-27 00:53:40 +0300

    Function Literal

--- js/ch4-functions.js
+++ js/ch4-functions.js
@@ -37,4 +37,34 @@ Mine were the very cipher of a function...
 * The thing that is special about function is that they can be invoked.
 */
 
+/* == Function Literal == */
+/*
+* Function objects are created within function literals:
+*/
+//create a variable called add and store a function in it that 
+//adds two numbers
+var add = function(a,b){
+	return a + b;
+}
+
+/*
+* A function has 4 parts:
+	(1) Reserved word function.
+	(2) Optional, function's name. The function can be use its name 
+		to call itself recursively. The name can also be used by debuggers and
+		development tools to identiy the function. If a function is not given
+		a name, like above, it is said to be anonymous.
+	(3) Set of paramenters, wrapped in parentheses. Unlike ordinary variables,
+		instead of being initialized to undefined, they will be initialized
+		to arguments supplied when the function is invoked.
+	(4) A set of statements wrapped in curly braces.
+
+* A function literal can appear anywhere that an expression can appear. 
+* Functions can be defined inside of other functions. An inner function
+ 	enjoys access to the parameters and variables of functions it is nested
+ 	within.
+* The function object created by a function literal contains a link to that
+	outer context. This is called closure. This is the source of enomous
+	expressive power.
+*/
 
