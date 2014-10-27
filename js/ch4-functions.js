8ff8716fab6e79e2297215a4ebb9935bc7019333
ProfNandaa <profnandaa@gmail.com>
2014-10-28 00:31:58 +0300

    Exceptions

--- js/ch4-functions.js
+++ js/ch4-functions.js
@@ -258,6 +258,50 @@ console.log(sum(10,30,50,23,54));
 	instead.
 */
 
+/* == Exceptions, pg.32 == */
+/*
+* Exceptions are unsusual (but not completely unexpected) mishaps that
+	interfer with the normal flow of a program. When such a mishap is
+	detected, your program should throw an exception.
+*/
+
+var add = function (a,b){
+	if(typeof a !== 'number' || typeof b !== 'number'){
+		throw {
+			name: 'TypeError',
+			message: 'add needs numbers'
+		};
+	}
+	return a + b;
+}
+
+/*
+* The throw statement interrupts execution of the function. It should
+	be given an `exception` object containing a `name` property that
+	identifies the type of the exception, and a descriptive `message`
+	property. You can also add other properties.
+* The `exception` object will be delivered to the `catch` clause of 
+	a `try` statement:
+*/
+
+//Make a try_it function that calls the new add function incorrectly
+
+var try_it = function(){
+	try{
+		add("seven");
+	}catch(e){
+		console.log(e.name + " : " + e.message);
+	}
+}
+
+try_it();
+
+/*
+* If an exception is thrown within a `try` block, control will go to
+	its `catch` clause.
+*/
+
+
 
 
 
