c2541d4335ed5dac967f3268d6745ac70c5398aa
ProfNandaa <profnandaa@gmail.com>
2014-11-01 13:21:30 +0300

    Memoization

--- chapters/ch4-functions.js
+++ chapters/ch4-functions.js
@@ -755,3 +755,95 @@ Function.method('curry',function(){
 
 var add1 = add.curry(1);
 console.log(add1(6));
+
+/* == Memoization == */
+/*
+* Functions can use objects to remember the results
+	of previous operations, making it possible to avoid
+	unnecesasry work.
+* e.g. We want a recursive function to compute
+	Fibonacci number:
+*/
+
+var fibonnaci = function(n){
+	return n < 2 ? n : fibonnaci(n-1) + fibonnaci(n-2);
+};
+
+for (var i=0; i<=10; i+=1){
+	console.log('// '+ i + ': ' + fibonnaci(i));
+}
+
+/*
+* This works, but it is doing a lot of unnecessary work.
+	The fibonnaci functin is called 453 times. We call 
+	it 11 times, and it calls itself 442 times in computing
+	values that are probably already recently computed.
+* We will keep our memoized results in a memo array that
+	we can hide in a closure. When our function is called,
+	it first looks to see if it already knows the result.
+	If it does, it can immediately return it.
+*/
+
+var fibonnaci = function(){
+	var memo = [0,1];
+	var fib = function(n){
+		var result = memo[n];
+		if(typeof result !== 'number'){
+			result = fib(n-1) + fib(n-2);
+			memo[n] = result;
+		}
+		return result;
+	};
+	return fib;
+}();
+
+//redo
+
+for (var i=0; i<=10; i+=1){
+	console.log('// '+ i + ': ' + fibonnaci(i));
+}
+
+/*
+* The function returns the same results but it is
+	called only 29 times.
+
+* We can generalize this by making a function that helps
+	us make memoized functions. The memoizer function
+	will tkae an initial `memo` array and the `fundamental`
+	function. It reutns a shell function that manages
+	the memo store and that calls the `fundamental`
+	function as needed. We pass the shell function and
+	the function's parameter to the fundamental function:
+*/
+
+var memoizer = function(memo,fundamental){
+	var shell = function(n){
+		var result = memo[n];
+		if(typeof result !== 'number'){
+			result = fundamental(shell,n);
+			memo[n] = result;
+		}
+		return result;
+	};
+	return shell;
+};
+
+/*
+* We can now define fibonnaci with the memoizer,
+	providing the inittial memo array and fundamental
+	function:
+*/
+
+var fibonnaci = memoizer([0,1],function(shell,n){
+	return shell(n-1) + shell(n-2);
+});
+
+/*
+* E.g., to provide a memoizing factorial function,
+	we only need to supply the basic factorial formula:
+*/
+
+var factorial = memoizer([1,1],function(shell,n){
+	return n * shell(n-1);
+});
+
